173f95089637c39076f9e8b694313f984dc531bb
Damien <alary.damien81@gmail.com>
2023-11-24 14:47:55 +0100

    Contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mime\Email;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="app_contact")
+     */
+    public function index(Request $request, MailerInterface $mailer): Response
+    {
+        $form = $this->createForm(ContactFormType::class);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $data = $form->getData();
+            $content = $data['name'] . ' a envoyé le message : ' . $data['message'];
+            $email = (new Email())
+            ->from('noreply@mysite.com')
+            ->to('siteadmin@hotmail.fr')
+            ->subject('Message depuis le site internet')
+            ->html($content);
+            $mailer->send($email);
+            $this->addFlash('notice', 'Votre message à bien été envoyer');    
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+            'form' => $form->createView(),
+        ]);
+    }
+}
